6dec250dfdac4970288b4c5efe62215a8c953653
mikaelg <mikaelg@DESKTOP-N3F5VRE>
2017-03-19 02:10:12 +0100

    Initial checkin. ToInt ToNullableInt ToDouble ToNullableDouble Cleanup Replace

---
+++ StringExtensions.Test/StringExtensionTests.cs
@@ -0,0 +1,479 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringExtensions;
+
+namespace StringExtensions.Test
+{
+    [TestClass]
+    public class StringExtensionTests
+    {
+        #region ToInt
+
+        [TestMethod]
+        public void ToInt_Success()
+        {
+            string value = "1234";
+            int expected = 1234;
+
+            int result = value.ToInt();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ToInt_WithPlusSign_Success()
+        {
+            string value = "+1234";
+            int expected = 1234;
+
+            int result = value.ToInt();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ToInt_NegativeValue_Success()
+        {
+            string value = "-1234";
+            int expected = -1234;
+
+            int result = value.ToInt();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ToInt_WithSpace_Success()
+        {
+            string value = "1 234";
+            int expected = 1234;
+
+            int result = value.ToInt();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ToInt_WithHtmlNbsp_Success()
+        {
+            string value = "1&nbsp;234";
+            int expected = 1234;
+
+            int result = value.ToInt();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ToInt_WithHtmlNbspUpperCase_Success()
+        {
+            string value = "1&NBSP;234";
+            int expected = 1234;
+
+            int result = value.ToInt();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ToInt_WithNonBreakingSpace_Success()
+        {
+            string value = "1\u00A0234";
+            int expected = 1234;
+
+            int result = value.ToInt();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ToInt_InvalidStringWithDefaultValue_Success()
+        {
+            string value = "Hello world!";
+            int defaultValue = -1;
+            int expected = -1;
+
+            int result = value.ToInt(defaultValue);
+
+            Assert.AreEqual(expected, result);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ToInt_InvalidStringNoDefaultValue_Exception()
+        {
+            string value = "Hello world!";
+
+            int result = value.ToInt();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ToInt_DoubleValueDot_Exception()
+        {
+            string value = "1234.12";
+
+            int result = value.ToInt();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ToInt_DoubleValueComma_Exception()
+        {
+            string value = "1234,12";
+
+            int result = value.ToInt();
+        }
+
+        #endregion ToInt
+
+        #region ToNullableInt
+
+        [TestMethod]
+        public void ToNullableInt_EmptyString_Success()
+        {
+            string value = string.Empty;
+            int? expected = null;
+
+            int? result = value.ToNullableInt();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ToNullableInt_HtmlNonBreakingSpace_Success()
+        {
+            string value = "&nbsp;&NBSP;";
+            int? expected = null;
+
+            int? result = value.ToNullableInt();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void ToNullableInt_NullString_Exception()
+        {
+            string value = null;
+
+            int? result = value.ToNullableInt();
+        }
+
+        #endregion ToNullableInt
+
+        #region ToDouble
+
+        [TestMethod]
+        public void ToDouble_IntegerValue_Success()
+        {
+            string value = "1234";
+            double expected = 1234;
+
+            double result = value.ToDouble();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ToDouble_DoubleValueDot_Success()
+        {
+            string value = "1234.12";
+            double expected = 1234.12;
+
+            double result = value.ToDouble();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ToDouble_DoubleValueComma_Success()
+        {
+            string value = "1234,12";
+            double expected = 1234.12;
+
+            double result = value.ToDouble();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ToDouble_WithSpace_Success()
+        {
+            string value = "1 234.12";
+            double expected = 1234.12;
+
+            double result = value.ToDouble();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ToDouble_WithHtmlNbsp_Success()
+        {
+            string value = "1&nbsp;234.12";
+            double expected = 1234.12;
+
+            double result = value.ToDouble();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ToDouble_WithHtmlNbspUppderCase_Success()
+        {
+            string value = "1&NBSP;234.12";
+            double expected = 1234.12;
+
+            double result = value.ToDouble();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ToDouble_WithNonBreakingSpace_Success()
+        {
+            string value = "1\u00A0234.12";
+            double expected = 1234.12;
+
+            double result = value.ToDouble();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ToDouble_InvalidStringWithDefaultValue_Success()
+        {
+            string value = "Hello world!";
+            double defaultValue = -1;
+            double expected = -1;
+
+            double result = value.ToDouble(defaultValue);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ToDouble_InvalidStringNoDefaultValue_Exception()
+        {
+            string value = "Hello world!";
+
+            double result = value.ToDouble();
+        }
+
+        #endregion ToDouble
+
+        #region ToNullableDouble
+
+        [TestMethod]
+        public void ToNullableDouble_EmptyString_Success()
+        {
+            string value = string.Empty;
+            double? expected = null;
+
+            double? result = value.ToNullableDouble();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ToNullableDouble_HtmlNonBreakingSpace_Success()
+        {
+            string value = "&nbsp;&NBSP;";
+            double? expected = null;
+
+            double? result = value.ToNullableDouble();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void ToNullableDouble_NullString_Exception()
+        {
+            string value = null;
+
+            int? result = value.ToNullableInt();
+        }
+
+        #endregion ToNullableDouble
+
+        #region Cleanup
+
+        [TestMethod]
+        public void Cleanup_Single_Success()
+        {
+            string value = "Hello world!";
+            string expected = "Hello !";
+
+            string result = value.Cleanup("World");
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Cleanup_Multipe_Success()
+        {
+            string value = "Hello world!";
+            string expected = "Hello";
+
+            string result = value.Cleanup("!", "World");
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Cleanup_NoMatch_Success()
+        {
+            string value = "Hello world!";
+            string expected = "Hello world!";
+
+            string result = value.Cleanup("No match");
+
+            Assert.AreEqual(expected, result);
+        }
+
+        #endregion Cleanup
+
+        #region Replace
+
+        [TestMethod]
+        public void Replace_CaseSensitive_Success()
+        {
+            string value = "Hello WORLD!";
+            string expected = "Hello world!";
+
+            string result = value.Replace("WORLD", "world");
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Replace_CaseSensitiveNotFound_Success()
+        {
+            string value = "Hello WORLD!";
+            string expected = "Hello WORLD!";
+
+            string result = value.Replace("world", "world");
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Replace_CaseInsensitive_Success()
+        {
+            string value = "Hello WORLD!";
+            string expected = "Hello world!";
+
+            string result = value.Replace("world", "world", true);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Replace_ReplaceEmptyString_Exception()
+        {
+            string value = "Hello WORLD!";
+            string expected = "Hello WORLD!";
+
+            string result = value.Replace("", "world");
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Replace_CaseSensitiveReplaceEmptyString_Success()
+        {
+            string value = "Hello WORLD!";
+            string expected = "Hello WORLD!";
+
+            string result = value.Replace("", "world", false);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Replace_CaseInsensitiveReplaceEmptyString_Success()
+        {
+            string value = "Hello WORLD!";
+            string expected = "Hello WORLD!";
+
+            string result = value.Replace("", "world", true);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Replace_ReplaceNull_Success()
+        {
+            string value = "Hello WORLD!";
+            string expected = "Hello WORLD!";
+
+            string result = value.Replace(null, "world");
+
+            Assert.AreEqual(expected, result);
+        }
+
+
+        [TestMethod]
+        public void Replace_CaseSensitiveReplaceNull_Success()
+        {
+            string value = "Hello WORLD!";
+            string expected = "Hello WORLD!";
+
+            string result = value.Replace(null, "world", false);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Replace_CaseInsensitiveReplaceNull_Success()
+        {
+            string value = "Hello WORLD!";
+            string expected = "Hello WORLD!";
+
+            string result = value.Replace(null, "world", true);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Replace_ReplaceWithNull_Success()
+        {
+            string value = "Hello WORLD!";
+            string expected = "Hello !";
+
+            string result = value.Replace("WORLD", null);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Replace_CaseSensitiveMultipleInstances_Success()
+        {
+            string value = "Hello WORLD! There are many worlds out there";
+            string expected = "Hello people! There are many worlds out there";
+
+            string result = value.Replace("WORLD", "people");
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Replace_CaseInsensitiveMultipleInstances_Success()
+        {
+            string value = "Hello WORLD! There are many worlds out there";
+            string expected = "Hello people! There are many peoples out there";
+
+            string result = value.Replace("WORLD", "people", true);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        #endregion Replace
+    }
+}
---
+++ StringExtensions/StringExtensions.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExtensions
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Converts a string to an Int32 value. If a default value have been set no exception will be thrown
+        /// in case the conversion fails.
+        /// </summary>
+        /// <param name="value">The string value that is to be parsed</param>
+        /// <param name="defaultValue">An optional default value in case the conversion fails</param>
+        /// <returns>The Int32 representation of the string</returns>
+        public static int ToInt(this string value, int? defaultValue = null)
+        {
+            int result;
+
+            string temp = value
+                .ToLower()
+                .Cleanup(
+                    " ",        //Regular space 
+                    "\u00A0",   //Non breaking space
+                    "&nbsp;",   //HTML non breaking space
+                    "+"         //Plus signs
+                )
+                .Trim();
+
+            if (int.TryParse(temp, out result))
+            {
+                return result;
+            }
+
+            if (defaultValue.HasValue)
+            {
+                return defaultValue.Value;
+            }
+
+            throw new ArgumentException($"The value '{value}' can not be parsed to an integer.");
+        }
+
+        
+        public static int? ToNullableInt(this string value, int? defaultValue = null)
+        {
+            string temp = value
+                .ToLower()
+                .Cleanup(
+                    " ",        //Regular space 
+                    "\u00A0",   //Non breaking space
+                    "&nbsp;",   //HTML non breaking space
+                    "+"         //Plus signs
+                )
+                .Trim();
+
+            if (string.IsNullOrWhiteSpace(temp))
+            {
+                if (defaultValue.HasValue)
+                {
+                    return defaultValue.Value;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+            return temp.ToInt(defaultValue);
+        }
+        
+        /// <summary>
+        /// Converts a string to a double value. If a default value have been set no exception will be thrown
+        /// in case the conversion fails
+        /// </summary>
+        /// <param name="value">The string value that is to be parsed</param>
+        /// <param name="defaultValue">An optional default value in casae the conversion fails</param>
+        /// <returns>The double representation of the string</returns>
+        public static double ToDouble(this string value, double? defaultValue = null)
+        {
+            double result;
+
+            string temp = value
+                .ToLower()
+                .Replace(",", System.Threading.Thread.CurrentThread.CurrentUICulture.NumberFormat.NumberDecimalSeparator)
+                .Replace(".", System.Threading.Thread.CurrentThread.CurrentUICulture.NumberFormat.NumberDecimalSeparator)
+                .Cleanup(
+                    " ",        //Regular space 
+                    "\u00A0",   //Non breaking space
+                    "&nbsp;",   //HTML non breaking space
+                    "+"         //Plus signs
+                )
+                .Trim();
+
+            if (double.TryParse(temp, out result))
+            {
+                return result;
+            }
+
+            if (defaultValue.HasValue)
+            {
+                return defaultValue.Value;
+            }
+
+            throw new ArgumentException($"The value '{value}' can not be parsed to a double.");
+        }
+
+        public static double? ToNullableDouble(this string value, double? defaultValue = null)
+        {
+            string temp = value
+                .ToLower()
+                .Cleanup(
+                    " ",        //Regular space 
+                    "\u00A0",   //Non breaking space
+                    "&nbsp;",   //HTML non breaking space
+                    "+"         //Plus signs
+                )
+                .Trim();
+
+            if (string.IsNullOrWhiteSpace(temp))
+            {
+                if (defaultValue.HasValue)
+                {
+                    return defaultValue.Value;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+            return temp.ToDouble(defaultValue);
+        }
+
+
+        /// <summary>
+        /// Removes parts of the string. The order of the parts to be removed is important
+        /// </summary>
+        /// <param name="value">The string that is to be cleaned</param>
+        /// <param name="cleanupStrings">The strings that is to be removed from the original string</param>
+        /// <returns>A string without the cleanup elements</returns>
+        public static string Cleanup(this string value, params string[] cleanupStrings)
+        {
+            string temp = value;
+
+            foreach (string cleanupString in cleanupStrings)
+            {
+                temp = temp.Replace(cleanupString, string.Empty, true);
+            }
+
+            return temp.Trim();
+        }
+
+        /// <summary>
+        /// Replaces a part of the string with another value. The user can choose if the replace should be case insensitive
+        /// </summary>
+        /// <param name="value">The string where the replace is to be done</param>
+        /// <param name="replace">The value that is to be replaced</param>
+        /// <param name="replaceWith">The value that the old string should be replaced with</param>
+        /// <param name="caseSensitive">If the method should look for the replace word without regard to case</param>
+        /// <returns>The replaced string</returns>
+        public static string Replace(this string value, string replace, string replaceWith, bool ignoreCase = false)
+        {
+            if (string.IsNullOrEmpty(replace))
+            {
+                return value;
+            }
+
+            string temp = ignoreCase ? value.ToLower() : value;
+            string searchPattern = ignoreCase ? replace.ToLower() : replace;
+            string replaceValue = replaceWith ?? string.Empty;
+
+            StringBuilder sb = new StringBuilder();
+
+            int lastIndex = 0;
+            int index = temp.IndexOf(searchPattern);
+
+            while (index != -1)
+            {
+                sb.Append(value.Substring(lastIndex, index - lastIndex));
+                sb.Append(replaceValue);
+
+                index += replace.Length;
+                lastIndex = index;
+                index = temp.IndexOf(searchPattern, index);
+            }
+
+            sb.Append(value.Substring(lastIndex));
+
+            return sb.ToString();
+        }
+    }
+}
